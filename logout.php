681952b8ebf579009f9a048348296434bf490ec7
Aldi Maldi <taktau534@gmail.com>
2026-04-21 13:19:29 +0700

    add destroy keranjang di customer

--- logout.php
+++ logout.php
@@ -1,7 +1,14 @@
 <?php
 session_start();
+
+// 1. Hancurkan session user
 session_unset();
 session_destroy();
-header("Location: index.php");
+
+// 2. HANCURKAN COOKIE KERANJANG (Set waktunya ke masa lalu)
+setcookie('keranjang', '', time() - 3600, '/'); 
+
+// 3. Kembalikan ke halaman login
+header("Location: login.php");
 exit;
 ?>
