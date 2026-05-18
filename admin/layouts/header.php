408ff6e23b6dc10f707d78bfa730918867f9c0d1
Aldi Maldi <taktau534@gmail.com>
2026-05-19 01:14:31 +0700

    membenarkan path assets admin

--- admin/layouts/header.php
+++ admin/layouts/header.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Admin Sneakers</title>
   <!-- <link rel="shortcut icon" type="image/png" href="./assets/images/logos/favicon.png" /> -->
-  <link rel="stylesheet" href="././assets/css/styles.min.css" />
+  <link rel="stylesheet" href="../assets/css/styles.min.css" />
   <style>
     /* 1. Menarik Sidebar ke ujung paling atas layar */
     #main-wrapper[data-layout="vertical"][data-sidebar-position="fixed"] .left-sidebar {
